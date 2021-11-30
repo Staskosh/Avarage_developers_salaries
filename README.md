dc9ecb0e1d076e9b3b1454cb6c9ef1960762cf63
staskosh <staskoshenkov@rambler.ru>
2021-11-30 16:04:18 +0300

    Add tables print for salaries and README.md

---
+++ README.md
@@ -0,0 +1,53 @@
+##Programming vacancies compare
+
+This script will allow you to find out the average salary of software developers for different programming languages.
+The information is taken from public access on the [HeadHunter](http://hh.ru) and [Superjob](https:superjob.ru)
+
+##How to install
+
+- Download the program to your computer
+- Get the key to [Superjob API](https://api.superjob.ru )
+- Create a .env file and specify the Superjob secret key
+```buildoutcfg
+SJ_TOKEN = 'Here's the secret key'
+```
+- Create a [virtual environment](https://python-scripts.com/virtualenv) for the project
+- Install dependencies
+```bash
+pip install -r requirements.txt
+```
+- Run the script
+```bash
+python developers_salaries.py
+````
+##Project Goals
+
+The code is written for educational purposes on online-course for web-developers dvmn.org.
+
+
+
+##Сравниваем вакансии программистов
+
+Данный скрипт позволит узнать усредненную зарплату разработчиков ПО для разных языков программирования.
+Информация взята из публичного доступа на сайтах [HeadHunter](HH.ruhttps://hh.ru) и [Superjob](https:superjob.ru)
+
+##Как установить
+
+- Скачай программу себе на компьютер
+- Получите ключ к [API Superjob](https://api.superjob.ru)  
+- Создай файл .env и укажи секретный ключ Superjob 
+```buildoutcfg
+SJ_TOKEN = 'Тут секретный ключ'
+```
+- Создай [виртуальное окружение](https://python-scripts.com/virtualenv) для проекта
+- Установи зависимости
+```bash
+pip install -r requirements.txt
+```
+- Запусти скрипт
+```bash
+python developers_salaries.py
+```
+##Цель проекта
+
+Код написан в образовательных целях на онлайн-курсе для веб-разработчиков dvmn.org.
---
+++ developers_salaries.py
@@ -0,0 +1,151 @@
+import os
+from itertools import count
+
+import requests
+from dotenv import load_dotenv
+from terminaltables import AsciiTable
+
+load_dotenv()
+
+
+def predict_salary(salary_from, salary_to):
+    if not salary_from and salary_to:
+        return salary_to * 0.8
+    elif salary_from and not salary_to:
+        return salary_from * 1.2
+    return (salary_from + salary_to) / 2
+
+
+def get_sj_salaries(sj_secret_key, position):
+    sj_url = 'https://api.superjob.ru/2.0/vacancies/'
+    headers = {
+        'X-Api-App-Id': sj_secret_key
+    }
+    payload = {
+        'keyword': position,
+        'catalogues': 48,
+        'town': 4,
+    }
+    salary_sum = 0
+    vacancies_processed = 0
+    vacancies_found = 0
+    for page_number in count(0):
+        payload['page'] = page_number
+        response = requests.get(sj_url, headers=headers, params=payload)
+        response.raise_for_status()
+        page = response.json()
+        vacancies = page['objects']
+        vacancies_found = page['total']
+        for salary in vacancies:
+            if not salary['currency'] == 'rub':
+                continue
+            salary_from = salary['payment_from']
+            salary_to = salary['payment_to']
+            predicted_salary = predict_salary(salary_from, salary_to)
+            if predicted_salary:
+                salary_sum += predicted_salary
+                vacancies_processed += 1
+        if not page['more']:
+            break
+    return salary_sum, vacancies_processed, vacancies_found
+
+
+def get_hh_salary(position):
+    hh_url = 'https://api.hh.ru/vacancies'
+    payload = {
+        'text': position,
+        'only_with_salary': 'true',
+        'specialization': '1.221',
+        'period': '31',
+        'area': '1',
+    }
+    salary_sum = 0
+    vacancies_processed = 0
+    vacancies_found = 0
+    for page_number in count(0):
+        payload['page'] = page_number
+        response = requests.get(hh_url, params=payload)
+        response.raise_for_status()
+        page = response.json()
+        vacancies = page['items']
+        vacancies_found = page['found']
+        total_pages = page['pages']
+        for salary in vacancies:
+            if not salary['salary']:
+                continue
+            elif salary['salary']['currency'] == 'RUR':
+                salary_from = salary['salary']['from']
+                salary_to = salary['salary']['to']
+                predicted_salary = predict_salary(salary_from, salary_to)
+                if predicted_salary:
+                    salary_sum += predicted_salary
+                    vacancies_processed += 1
+        if page_number > total_pages:
+            break
+    return salary_sum, vacancies_processed, vacancies_found
+
+
+def get_average_hh_salaries(positions):
+    vacancies_info = {}
+    for position in positions:
+        salary_sum, vacancies_processed, vacancies_found = get_hh_salary(position)
+        vacancies_info[position] = {
+            'vacancies_found': vacancies_found,
+            'vacancies_processed': vacancies_processed,
+            'average_salary': int(salary_sum / vacancies_processed)
+        }
+    return vacancies_info
+
+
+def get_average_sj_salaries(positions, sj_secret_key):
+    vacancies_info = {}
+    for position in positions:
+        salary_sum,
+        vacancies_processed,
+        vacancies_found = get_sj_salaries(sj_secret_key, position)
+        vacancies_info[position] = {
+            'vacancies_found': vacancies_found,
+            'vacancies_processed': vacancies_processed,
+            'average_salary': int(salary_sum / vacancies_processed)
+        }
+    return vacancies_info
+
+
+def print_tables(sj_average_salaries, source):
+    table_data = [
+        [source, 'Moscow'],
+        [
+            'Язык программирования',
+            'Вакансий найдено',
+            'Вакансий обработано',
+            'Средняя зарплата',
+        ],
+    ]
+    for position, salary in sj_average_salaries.items():
+        position_salariy = [
+            position,
+            salary['vacancies_found'],
+            salary['vacancies_processed'],
+            salary['average_salary'],
+        ]
+        table_data.append(position_salariy)
+    table = AsciiTable(table_data)
+    print(table.table)
+
+
+def main():
+    positions = [
+        'Программист JavaScript', 'Программист Java',
+        'Программист Python', 'Программист PHP',
+        'Программист C++', 'Программист C#',
+        'Программист C', 'Программист Swift'
+        ]
+    hh_salary_statistics = get_average_hh_salaries(positions)
+    sj_secret_key = os.getenv('SJ_TOKEN')
+    sj_salary_statistics = get_average_sj_salaries(positions, sj_secret_key)
+    print_vacancies = print_tables(sj_salary_statistics, 'Superjob')
+    print_vacancies = print_tables(hh_salary_statistics, 'HeadHunter')
+
+
+if __name__ == '__main__':
+    main()
